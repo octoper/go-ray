1c805daa0539568629d983d6a375e4eca694106f
Vaggelis Yfantis <me@octoper.me>
2021-02-28 17:40:49 +0200

    wip

--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.15
 require (
 	github.com/bradleyjkemp/cupaloy/v2 v2.6.0
 	github.com/google/uuid v1.2.0
+	github.com/stretchr/testify v1.7.0 // indirect
 	golang.org/dl v0.0.0-20210220033039-562909534da3 // indirect
 )
--- ray_test.go
+++ ray_test.go
@@ -2,6 +2,7 @@ package ray
 
 import (
 	"github.com/bradleyjkemp/cupaloy/v2"
+	"github.com/stretchr/testify/assert"
 	"os"
 	"testing"
 )
@@ -13,29 +14,74 @@ func TestMain(m *testing.M) {
 }
 
 func Test_CanSendAStringToRay(t *testing.T) {
-	t.Parallel()
-
 	ray := Ray("hey")
 
 	result, _ := ray.SentJsonPayloads()
 
-	err := cupaloy.Snapshot(result)
-
-	if err != nil {
-		t.Fatal("Tests in different packages are independent of each other", err)
-	}
+	cupaloy.SnapshotT(t, result)
 }
 
 func Test_CanSendAnArrayToRay(t *testing.T) {
-	t.Parallel()
-
 	ray := Ray([]int{2,4,6}).Color("green")
 
 	result, _ := ray.SentJsonPayloads()
 
-	err := cupaloy.Snapshot(result)
-
-	if err != nil {
-		t.Fatal("Tests in different packages are independent of each other", err)
-	}
+	cupaloy.SnapshotT(t, result)
 }
+
+func TestApplication_Bool(t *testing.T) {
+
+	ray := Ray().Bool(false)
+
+	result, _ := ray.SentJsonPayloads()
+
+	cupaloy.SnapshotT(t, result)
+}
+
+func TestApplication_Ban(t *testing.T) {
+
+	ray := Ray().Ban()
+
+	result, _ := ray.SentJsonPayloads()
+
+	cupaloy.SnapshotT(t, result)
+}
+
+
+func TestApplication_SetHost(t *testing.T) {
+	t.Parallel()
+
+	Ray().SetHost("192.168.1.255")
+
+	assert.Equal(t, "192.168.1.255", Ray().Host())
+}
+
+
+func TestApplication_SetPort(t *testing.T) {
+	t.Parallel()
+
+	Ray().SetPort(27154)
+
+	assert.Equal(t, 27154, Ray().Port())
+}
+
+func TestApplication_Color(t *testing.T) {
+	t.Parallel()
+
+	ray := Ray().Color("green")
+
+	result, _ := ray.SentJsonPayloads()
+
+	cupaloy.SnapshotT(t, result)
+}
+
+func TestApplication_Red(t *testing.T) {
+	t.Parallel()
+
+	ray := Ray().Red()
+
+	result, _ := ray.SentJsonPayloads()
+
+	cupaloy.SnapshotT(t, result)
+}
+
