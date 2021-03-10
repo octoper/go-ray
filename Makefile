ebe36b6fac7feacdc65739d6b67204a36e97114e
Vaggelis Yfantis <me@octoper.me>
2021-03-11 01:33:10 +0200

    wip

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
-build:
+build_playground: clean_playground
 	go build -o playground/bin/main playground/playground.go
 
-run:
+playground:
 	go run playground/playground.go
+
+clean_playground:
+	rm -R playground/bin/
--- playground/playground.go
+++ playground/playground.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"time"
 	"github.com/octoper/go-ray"
 )
 
 func main() {
-	ray.Ray("hey").ClearAll()
-	ray.Ray("heyyy")
+	ray.Ray().ClearAll()
+	myRay := ray.Ray(4)
+	for i := 0; i < 10; i++ {
+		time.Sleep(1)
+		myRay.Send(2)
+	}	
 }
