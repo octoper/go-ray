ec6010a9069d2caefdbf8a7391f6af727bc4e6d6
Vaggelis Yfantis <me@octoper.me>
2021-03-11 02:50:45 +0200

    wip

--- ray.go
+++ ray.go
@@ -236,7 +236,7 @@ func (r *application) Pause() *application {
 	r.SendRequest(payloads.NewCreateLockPayload(hex.EncodeToString(lockName)))
 
 	for {
-		time.Sleep(5); //nolint
+		time.Sleep(8 * time.Second);
 
 		lockExistsClient := r.Client().LockExists(hex.EncodeToString(lockName))
 		if !lockExistsClient.Active {
