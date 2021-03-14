7874caa2e47a37123d791d2defe4c6b658e45bb2
Vaggelis Yfantis <me@octoper.me>
2021-03-14 04:26:33 +0200

    wip

--- payloads/ClearAllPayload.go
+++ payloads/ClearAllPayload.go
@@ -1,6 +1,6 @@
 package payloads
 
-// NewClearAllPayload creates a new Cleal All Payload
+// NewClearAllPayload creates a new Clear All Payload
 func NewClearAllPayload() Payload {
 	return Payload {
 		Type: "clear_all",
